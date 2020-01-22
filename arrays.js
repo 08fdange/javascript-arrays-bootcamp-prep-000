1f8338f0514c2ba6f2b92668f69bdc7ed0dd9a58
Frank  D'Angelo <08fdange@gmail.com>
2020-01-22 07:18:19 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  return[element, ...array];
+  
+}
+
+//addElementToBeginningOfArray(chocolateBars, "hersheys bar")
+//console.log(chocolateBars)
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return(array = array.unshift(element))
+  
+}
+
+function addElementToEndOfArray(array, element) {
+  return[...array, element]
+}
+//function destructivelyAddElementToEndOfArray()
+
+//function destructivelyRemoveElementFromBeginningOfArray() {
+//} 
